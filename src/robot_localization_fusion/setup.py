63587cc5aff176a76bb11d71061dbe1a7e2dc6e3
Prajwal Gahanai <prajwal.s@gahanai.com>
2026-01-30 16:16:36 +0530

    Updated launch file

--- src/robot_localization_fusion/setup.py
+++ src/robot_localization_fusion/setup.py
@@ -10,6 +10,8 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+    ('share/' + package_name + '/config', ['config/ekf_localization.yaml']),
+    ('share/' + package_name + '/launch', ['launch/localization.py']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -28,3 +30,4 @@ setup(
         ],
     },
 )
+
